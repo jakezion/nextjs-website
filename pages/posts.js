dc3b89382a05b1f5c27eede88e11c2261bca7dd8
jakezion <jakesumner@live.co.uk>
2022-01-29 19:53:11 +0000

    init commit

---
+++ pages/posts.js
@@ -0,0 +1,69 @@
+import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
+import Layout from '../components/layouts/article'
+import Section from '../components/section'
+import { GridItem } from '../components/grid-item'
+
+import thumbFishWorkflow from '../public/images/contents/ed.jpg'
+import thumbMyDeskSetup from '../public/images/contents/ed.jpg'
+import thumb500PaidUsers from '../public/images/contents/ed.jpg'
+import thumbFinancialGoal from '../public/images/contents/ed.jpg'
+import thumbHowToPriceYourself from '../public/images/contents/ed.jpg'
+import thumb50xFaster from '../public/images/contents/ed.jpg'
+
+const Posts = () => (
+  <Layout title="Posts">
+    <Container>
+      <Heading as="h3" fontSize={20} mb={4}>
+        Popular Posts
+      </Heading>
+
+      <Section delay={0.1}>
+        <SimpleGrid columns={[1, 2, 2]} gap={6}>
+          <GridItem
+            title="temp"
+            thumbnail={thumbFishWorkflow}
+            href="https://www.youtube.com/"
+          />
+          <GridItem
+            title="temp"
+            thumbnail={thumbMyDeskSetup}
+            href="https://www.youtube.com/"
+          />
+        </SimpleGrid>
+      </Section>
+
+      <Section delay={0.3}>
+        <SimpleGrid columns={[1, 2, 2]} gap={6}>
+          <GridItem
+            title="temp"
+            thumbnail={thumb500PaidUsers}
+            href="https://www.youtube.com/"
+          />
+          <GridItem
+            title="temp"
+            thumbnail={thumbFinancialGoal}
+            href="https://www.youtube.com/"
+          />
+        </SimpleGrid>
+      </Section>
+
+      <Section delay={0.5}>
+        <SimpleGrid columns={[1, 2, 2]} gap={6}>
+          <GridItem
+            title="temp"
+            thumbnail={thumbHowToPriceYourself}
+            href="https://www.youtube.com/"
+          />
+          <GridItem
+            title="temp"
+            thumbnail={thumb50xFaster}
+            href="https://www.youtube.com/"
+          />
+        </SimpleGrid>
+      </Section>
+    </Container>
+  </Layout>
+)
+
+export default Posts
+export { getServerSideProps } from '../components/chakra'
