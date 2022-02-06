c0d4672415435b636ff55711e542a443cb40e5ad
jakezion <jakesumner@live.co.uk>
2022-02-06 16:19:18 +0000

    update metadata

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -11,16 +11,16 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Takuya's homepage" />
-        <meta name="author" content="Takuya Matsuyama" />
-        <meta name="author" content="craftzdog" />
+        <meta name="description" content="Jake's homepage" />
+        <meta name="author" content="Jake Sumner" />
+        <meta name="author" content="jakezion" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@craftzdog" />
-        <meta name="twitter:creator" content="@craftzdog" />
+        <meta name="twitter:site" content="@JakeSumner_11" />
+        <meta name="twitter:creator" content="@JakeSumner_11" />
         <meta name="twitter:image" content="/card.png" />
-        <meta property="og:site_name" content="Takuya Matsuyama's Homepage" />
+        <meta property="og:site_name" content="Jake Sumners's Homepage" />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="/card.png" />
         <title>Jake Sumner - Homepage</title>
