7a5375dfacdd8fbd0fd71a1b4ac99d466871da3a
jake <jakesumner@live.co.uk>
2022-02-15 13:36:22 +0000

    update

--- pages/404.js
+++ pages/404.js
@@ -1,27 +1,27 @@
 import NextLink from 'next/link'
 import {
-  Box,
-  Heading,
-  Text,
-  Container,
-  Divider,
-  Button
+    Box,
+    Heading,
+    Text,
+    Container,
+    Divider,
+    Button, useColorModeValue
 } from '@chakra-ui/react'
 
 const NotFound = () => {
-  return (
-    <Container>
-      <Heading as="h1">Not found</Heading>
-      <Text>The page you&apos;re looking for was not found.</Text>
-      <Divider my={6} />
-      <Box my={6} align="center">
-        <NextLink href="/">
-          <Button colorScheme="teal">Return to home</Button>
-        </NextLink>
-      </Box>
-    </Container>
-  )
+    return (
+        <Container>
+            <Heading as="h1" ailgn="center">Not found</Heading>
+            <Text>The page you&apos;re looking for was not found.</Text>
+            <Divider my={6} />
+            <Box my={6} align="center">
+                <NextLink href="/">
+                    <Button color={useColorModeValue("white","black")} bg={useColorModeValue("monoPurple","monoYellow")}>Return to home</Button>
+                </NextLink>
+            </Box>
+        </Container>
+    )
 }
 
 export default NotFound
-export { getInitialProps } from '../components/chakra'
+// export { getInitialProps } from '../components/chakra'
